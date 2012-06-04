a0d733271efd73d5220738f5c433d29c66e70964
gaojun <gaojun@zhenzhi.com>
2012-06-04 12:12:22 +0800

    1

--- class/mvc/model/DeleteTester.php
+++ class/mvc/model/DeleteTester.php
@@ -9,14 +9,7 @@ class DeleteTester extends ControlPanel
 {
 	public function process()
 	{
-		Model::create('frameworktest:book')
-        		//->hasOne('frameworktest:author','bid','aid')
-        		//->hasMany('frameworktest:bookcomment','bid','bid')
-				->delete(
-					"frameworktest_book.bid = 2",
-			        'frameworktest_book.bid',
-			        '20'
-				) ;
+		Model::create('frameworktest:book')->delete("frameworktest_book.bid = 2") ;
 		exit;
 	}
 }
